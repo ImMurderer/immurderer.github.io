8861c1677fc97f4c498d9b7aaec807e48c69f16f
ImMurderer <joaopedro0128@hotmail.com>
2020-05-30 05:57:22 -0300

    feat: website deploy
    Signed-off-by: ImMurderer <joaopedro0128@hotmail.com>

---
+++ build/precache-manifest.43f2090d9f8f88648994068d09a5a40b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706379a6a24e4e574ccaf3fc91d404b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dd2ffaede19e681ba7e",
+    "url": "/static/js/2.f278fbb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f278fbb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de63b41c0b8e7b7b023f",
+    "url": "/static/js/main.a22843c2.chunk.js"
+  },
+  {
+    "revision": "78f9edbd0ad0849523a9",
+    "url": "/static/js/runtime-main.fb25862d.js"
+  }
+]);
