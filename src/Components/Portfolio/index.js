507e0779ffb8ed6d21d29b94afaf244270d92588
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-27 19:30:21 -0300

    fix: made updates on projets and added links to hosted projects
    Signed-off-by: Joao Pedro Prado Leal <joaopedro0128@hotmail.com>

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -10,19 +10,35 @@ const bestProjects = [
     "name": "Big Game Survey",
     "img": "img/BGS.png",
     "description": "Website for collecting data about game preferences",
-    "techs": ["Java", "Spring", "JS", "React", "React Native"]
+    "techs": ["Java", "Spring", "JS", "React", "React Native"],
+    "link": "https://sds1-joaop.netlify.app/"
   },
   {
     "name": "Room - Frontend Mentor",
     "img": "img/Room.png",
     "description": "Design prompt from frontendmentors.io",
-    "techs": ["JS", "React", "SASS", "Webpack"]
+    "techs": ["JS", "React", "SASS", "Webpack"],
+    "link": "https://room-joaop.netlify.app/"
   },
   {
     "name": "Bord8",
     "img": "img/Bord8.png",
-    "description": "Tool to make complex shapes with css border propertie.",
-    "techs": ["JS", "HTML", "CSS"]
+    "description": "Tool to make complex shapes with css border property.",
+    "techs": ["JS", "HTML", "CSS"],
+    "link": "https://jppradoleal.github.io/Bord8"
+  },
+  {
+    "name": "Happy",
+    "img": "img/Happy.png",
+    "description": "System that brings together jacareí orphanages that receive visits, presenting information such as contact, visiting instructions, images, etc..",
+    "techs": ["JS", "HTML", "CSS", "ReactJS", "ExpressJS", "PostgreSQL", "TypeORM", "React Native"],
+    "link": "https://nlw-joaop.netlify.app/"
+  },{
+    "name": "AirCNC",
+    "img": "img/AirCNC.png",
+    "description": "Startup that aims to connect Developers and Enterprises.",
+    "techs": ["JS", "HTML", "CSS", "ExpressJS", "NodeJS", "ReactJS", "React Native", "MongoDB", "SocketIO"],
+    "link": "http://omni9-joaop.netlify.app/"
   }
 ]
 
@@ -51,7 +67,7 @@ const Portfolio = () => {
             <div className="description">
               <h2>
                 <a
-                  href={item.html_url}
+                  href={item.link}
                   rel="noopener noreferrer"
                   target="_blank"
                 >
